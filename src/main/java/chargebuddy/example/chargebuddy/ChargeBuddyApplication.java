52d30ea3a99bbf5f63e1026541e2f131ab11ff2e
hansol-01 <gksthgus2022@gmail.com>
2025-08-12 19:50:28 +0900

    빌드

---
+++ src/main/java/chargebuddy/example/chargebuddy/ChargeBuddyApplication.java
@@ -0,0 +1,13 @@
+package chargebuddy.example.chargebuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargeBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChargeBuddyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/chargebuddy/example/chargebuddy/ChargeBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package chargebuddy.example.chargebuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargeBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
