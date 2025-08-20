fd49df8f30078a8df44cfebaf3e840bf64c02af4
hansol-01 <gksthgus2022@gmail.com>
2025-08-20 19:45:25 +0900

    필요없는import 수정

--- src/main/java/chargebuddy/example/chargebuddy/ChargingStationInfo/Repository/OperatingRepository.java
+++ src/main/java/chargebuddy/example/chargebuddy/ChargingStationInfo/Repository/OperatingRepository.java
@@ -3,7 +3,6 @@ package chargebuddy.example.chargebuddy.ChargingStationInfo.Repository;
 import chargebuddy.example.chargebuddy.ChargingStationInfo.Domain.Operating;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface OperatingRepository extends JpaRepository<Operating, String> {
