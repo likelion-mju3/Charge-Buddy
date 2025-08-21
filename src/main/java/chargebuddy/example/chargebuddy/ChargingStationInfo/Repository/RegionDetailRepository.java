684a02fc5206b19b3474d0c2de783479b8fde20b
hansol-01 <gksthgus2022@gmail.com>
2025-08-22 04:20:09 +0900

    batch등 수정

--- src/main/java/chargebuddy/example/chargebuddy/ChargingStationInfo/Repository/RegionDetailRepository.java
+++ src/main/java/chargebuddy/example/chargebuddy/ChargingStationInfo/Repository/RegionDetailRepository.java
@@ -3,7 +3,6 @@ package chargebuddy.example.chargebuddy.ChargingStationInfo.Repository;
 import chargebuddy.example.chargebuddy.ChargingStationInfo.Domain.RegionDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 public interface RegionDetailRepository extends JpaRepository<RegionDetail, String> {
 
--- src/main/java/chargebuddy/example/chargebuddy/ChargingStationInfo/Service/RegionDetailService.java
+++ src/main/java/chargebuddy/example/chargebuddy/ChargingStationInfo/Service/RegionDetailService.java
@@ -1,14 +1,9 @@
 package chargebuddy.example.chargebuddy.ChargingStationInfo.Service;
 
-import chargebuddy.example.chargebuddy.ChargingStationInfo.Domain.RegionDetail;
 import chargebuddy.example.chargebuddy.ChargingStationInfo.Repository.RegionDetailRepository;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
