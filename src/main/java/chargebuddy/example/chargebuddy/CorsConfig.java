73329c6a40fe5799fcef5551cfdfc0a97a9e0b7c
hansol-01 <gksthgus2022@gmail.com>
2025-08-24 19:06:43 +0900

    cors 수정

--- src/main/java/chargebuddy/example/chargebuddy/CorsConfig.java
+++ src/main/java/chargebuddy/example/chargebuddy/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:8080",
+                .allowedOrigins("http://localhost:8080",
                         "http://chargebuddy.digital",
                         "https://chargebuddy.digital")
                 .allowedMethods("*")
